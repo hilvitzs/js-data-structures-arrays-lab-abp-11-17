147c02132073798ce3a703bdd200055bc5768e25
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-09 01:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ function prependDriver(name) {
 function removeLastDriver() {
   return members = drivers.slice(0, 2);
 }
+
+function removeFirstDriver() {
+  return members = drivers.slice(0, 1);
+}
