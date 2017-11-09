b5b757f1f0f6a6ad494dc90d802f342c77a59a9b
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-09 01:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function destructivelyRemoveFirstDriver(name) {
 }
 
 function appendDriver(name) {
-  return members = ["Arnold", ...drivers.splice()]
+  return members = ["Arnold", ...drivers.slice()]
 }
